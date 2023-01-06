07811107dca2fb45a0e161eb3058837555b7b7ea
Adam nicinski <adamnicinski@hotmail.com>
2023-01-06 19:19:41 +0100

    https://github.com/nita14/TAN---Zadanie-4

---
+++ WpfExample/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using WpfExample.Models;
+using WpfExample.ViewModels;
+
+namespace WpfExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new MainWindowViewModel();
+
+            DataContext = _viewModel;
+
+            _viewModel.Students.Add(new Student
+            {
+                IdStudent = 1,
+                IndexNumber = "s1234",
+                FirstName = "John",
+                LastName = "Smith",
+                Status = "Skreślony",
+                BirthDate = new DateTime(1980, 12, 20),
+                PhotoPath = @"C:\Users\Piotr\OneDrive - Polsko-Japońska Akademia Technik Komputerowych\Materiały\Przedmioty\2021_2022_zimowy\TAN\WpfExample\WpfExample\Images\photo1.jpg"
+            });
+            _viewModel.Students.Add(new Student
+            {
+                IdStudent = 2,
+                IndexNumber = "s1422",
+                FirstName = "Andrzej",
+                LastName = "Malewski",
+                Status = "Aktywny",
+                BirthDate = new DateTime(1982, 5, 10),
+                PhotoPath = @"C:\Users\Piotr\OneDrive - Polsko-Japońska Akademia Technik Komputerowych\Materiały\Przedmioty\2021_2022_zimowy\TAN\WpfExample\WpfExample\Images\photo2.jpg"
+            });
+            _viewModel.Students.Add(new Student
+            {
+                IdStudent = 3,
+                IndexNumber = "s5422",
+                FirstName = "Krzysztof",
+                LastName = "Kowalewicz",
+                Status = "Skreślony",
+                BirthDate = new DateTime(1991, 8, 5),
+                PhotoPath = @"C:\Users\Piotr\OneDrive - Polsko-Japońska Akademia Technik Komputerowych\Materiały\Przedmioty\2021_2022_zimowy\TAN\WpfExample\WpfExample\Images\photo3.jpg"
+            });
+            _viewModel.Students.Add(new Student
+            {
+                IdStudent = 4,
+                IndexNumber = "s5322",
+                FirstName = "Marcin",
+                LastName = "Maciejewski",
+                Status = "Aktywny",
+                BirthDate = new DateTime(1985, 9, 20),
+                PhotoPath = @"C:\Users\Piotr\OneDrive - Polsko-Japońska Akademia Technik Komputerowych\Materiały\Przedmioty\2021_2022_zimowy\TAN\WpfExample\WpfExample\Images\photo4.jpg"
+            });
+        }
+
+        private void DodajStudentaButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new AddStudentWindow();
+            dialog.ShowDialog();
+
+            var st = dialog.GetStudent();
+            if (st != null)
+            {
+                _viewModel.Students.Add(st);
+            }
+        }
+    }
+}
